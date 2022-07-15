a27ed48fd4194e95ff206e6303ae4dce1cb3d95a
Luc de Wit <luc.dewit@hotmail.nl>
2022-07-15 16:12:53 +0200

    Download page

--- output/script.js
+++ output/script.js
@@ -7,3 +7,28 @@ const md = new markdownit();
     const mdres = md.render(markdown.innerHTML.replace(/\&gt\;/g, ">"));
     markdown.innerHTML = mdres;
 });
+
+const downloadTemplate = `
+<tr>
+    <td>Version {0}</td>
+    <td>
+        <button onclick="window.open('https://github.com/justlucdewit/cod/archive/refs/tags/{0}.zip','_blank')">Download</button>
+    </td>
+    <td>
+        <button onclick="window.open('https://github.com/justlucdewit/cod/releases/download/{0}/cod.exe','_blank')">Download</button>
+    </td>
+    <td class="disabled">
+        <button>Download</button>
+    </td>
+    <td class="disabled">
+        <button>Download</button>
+    </td>
+</tr>`
+
+const versions = ['v1.0.0'];
+
+for (const version of versions) {
+    const downloadTable = document.getElementById('downloads-table-content');
+    downloadTable.innerHTML += downloadTemplate
+        .replace(/\{0\}/g, version);
+}
