22306865b2d48969d80c65431944bcc6aab5a49c
Luc de Wit <luc.dewit@hotmail.nl>
2022-07-16 17:32:43 +0200

    Open the compile API endpoint for everyone

--- backend/compile.php
+++ backend/compile.php
@@ -1,4 +1,11 @@
 <?php
     // api.codlang.com/compile
+    
+    // Open the API end point to anyone
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: *");
+    header("Access-Control-Allow-Headers: *");
+
+    // Test result
     echo "test";
 ?>
